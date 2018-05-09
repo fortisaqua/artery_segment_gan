bff19abb4ae3280cad51d9581f7b30ab802ff88b
fortisaqua <1336250263@qq.com>
2018-05-09 11:09:25 +0800

    repaired data transfering bug during full testing

--- tools.py
+++ tools.py
@@ -35,7 +35,7 @@ class Test_data():
         self.image_array = np.transpose(self.image_array,[2,1,0])
         self.image_shape = np.shape(self.image_array)
         if "airway" in type:
-            self.image_array = np.int16(self.image_shape < 0) * self.image_shape
+            self.image_array = np.int16(self.image_array < 0) * self.image_array
         self.block_shape=block_shape
         self.blocks=dict()
         self.results=dict()
